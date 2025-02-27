3346728165722771ad30c6b92f0a4b4856861345
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-28 01:03:09 +0200

    dubai: disable brightness slider gamma conversion
    Change-Id: Ia9a9d8e98053bcdbfccbee5fe2bdb0b030e7b60f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,6 +35,7 @@ BOARD_SUPER_PARTITION_SIZE := 8925478912
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
